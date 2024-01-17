3e81ae04c4f876e4dd8cf24079977e977aa8cd24
Daniel Román Terrón Pérez <135791777+DrTerron@users.noreply.github.com>
2024-01-16 21:47:49 -0600

    Update db_models.py

--- db_models.py
+++ db_models.py
@@ -6,10 +6,9 @@ import math
 from datetime import datetime
 from database import db
 
-class Prediction(db.Model):
-    """ Una predicción en la base de datos.
-    """
-    __tablename__ = 'prediction'  # Nombre de la tabla en la base de datos
+class Estimacion(db.Model):
+    """ Una estimación en la base de datos.    """
+    __tablename__ = 'estimacion'  # Nombre de la tabla en la base de datos
 
     prediction_id = db.Column('id', db.Integer, primary_key=True)
 
@@ -17,15 +16,14 @@ class Prediction(db.Model):
     sepal_width = db.Column('sepal_width', db.Float, nullable=False)
     petal_length = db.Column('petal_length', db.Float, nullable=False)
     petal_width = db.Column('petal_width', db.Float, nullable=False)
-    predicted_class = db.Column('class', db.Text, nullable=False)
+    predicted_tipo = db.Column('class', db.Text, nullable=False)
     # score = db.Column('score', db.Float, nullable=False)
     created_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     # -----------------------------------------------------------------------------------
     def __init__(self, representation=None):
-        """ Construye una Prediccion nueva usando su representación REST
-        """
-        super(Prediction, self).__init__()
+        """ Construye una estimación nueva usando su representación REST  """
+        super(Estimacion, self).__init__()
         self.sepal_length = representation.get('sepal_length')
         self.sepal_width = representation.get('sepal_width')
         self.petal_length = representation.get('petal_length')
@@ -33,11 +31,10 @@ class Prediction(db.Model):
 
     # -----------------------------------------------------------------------------------
     def __repr__(self):
-        """ Convierte una Predicción a una cadena de texto
-        """
-        template_str = '<Prediction [{}]: sepal_length={}, sepal_width={}, petal_length={}, petal_width={}, class={}>'
+        """ Convierte una estimación a una cadena de texto  """
+        template_str = '<Estimacion [{}]: sepal_length={}, sepal_width={}, petal_length={}, petal_width={}, tipo={}>'
         return template_str.format(
             str(self.prediction_id) if self.prediction_id else 'NOT COMMITED', 
             self.sepal_length, self.sepal_width, self.petal_length, self.petal_width,
-            self.predicted_class or 'No calculado'
+            self.predicted_tipo or 'No calculado'
         )
